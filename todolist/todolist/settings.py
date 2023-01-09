ec787aaa5aeb19bd29e9d7efcdd5ad05703e2082
Dmitry Reznikov <Ontaelio@users.noreply.github.com>
2023-01-09 17:04:48 +0400

    done

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -22,6 +22,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -110,3 +111,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core.User'
