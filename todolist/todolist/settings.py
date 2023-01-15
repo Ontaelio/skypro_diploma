61e11a2be6528ee4bdd60ddce3b6d71ebea99656
Dmitry Reznikov <indraputra@yandex.ru>
2023-01-15 21:41:03 +0300

    Lesson 34 (#2)
    * containers
    
    * containers-front
    
    * Static finally
    
    * build and deploy
    
    * build and deploy
    
    * build and deploy
    
    * try_deploy
    
    * try_deploy
    
    * try_deploy
    
    * deploy and run
    
    * deploy and run
    
    * deploy and run
    
    * deploy and run
    
    * deploy and run
    
    * deploy and run
    
    * deploy and run
    
    * deploy and run
    
    * deploy and run
    
    * deploy and run
    
    * deploy and run
    
    * deploy and run
    
    * deploy and run
    
    * deploy and run
    
    * deploy and run
    
    * deploy and run
    
    * deploy and run
    
    * deploy and run
    
    * deploy and run
    
    * deploy and run
    
    * deploy and run
    
    * deploy and run
    
    * deploy and run
    
    * deploy and run
    
    * deploy and run
    
    * deploy and run
    
    * deploy and run
    
    * deploy and run
    
    * deploy and run
    
    * deploy and run
    
    * deploy and run
    
    * deploy and run
    
    * deploy and run
    
    * deploy and run
    
    * deploy and run
    
    * deploy and run
    
    Co-authored-by: Dmitry Reznikov <Ontaelio@users.noreply.github.com>

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -101,14 +101,8 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
-
-STATIC_URL = 'static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR.joinpath('static')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
