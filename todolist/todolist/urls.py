7926c95262f9bb4bf4d232c0a5336770c9a1a050
Dmitry Reznikov <Ontaelio@users.noreply.github.com>
2023-01-20 18:09:10 +0400

    VK added

--- todolist/todolist/urls.py
+++ todolist/todolist/urls.py
@@ -20,7 +20,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('core/', include('core.urls'))
+    path('core/', include('core.urls')),
+    path('oauth/', include('social_django.urls', namespace='social')),
 ]
 
 if settings.DEBUG:
